fa9457faac028b95f4eb6d92fe52bf3c1ce8da55
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-08-30 12:31:41 +0900

    chore(js): create operate function
    operate function:
    - takes an operator and 2 numbers
    - calls add/subtract/multiply/divide function

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+function operate(a, operator, b) {
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = add(a, b);
+      break;
+    case "-":
+      result = subtract(a, b);
+      break;
+    case "*":
+      result = multiply(a, b);
+      break;
+    case "/":
+      result = divide(a, b);
+      break;
+    default:
+      result = "uuh";
+      break;
+  }
+
+  return result;
+}
+
 function add(a, b) {
   return a + b;
 }
@@ -13,3 +37,5 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+console.log(operate(25, "+", 7));
