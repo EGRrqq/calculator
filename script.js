c80ab2962151601d08e19acc777fc67b8c80d8e6
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-08-31 14:31:44 +0900

    chore(js): add history for user calculations
    - all nodes pop up in .result-display
    
    chore(css): styles for the .result-display
    - add gap
    - change height to min-height

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// wrap up input with container
-// add div.textContent '='
-// add div.textContent = result of operate() function
+// add history for user calculations
+// - all nodes need pop up in .result-display
+// - after user clicks on 'equal' button input.value = calcValues.result
+// after the user clicks on 'equal' button, all calcValues except result should be set to ''
 
 const calcValues = {
   a: "",
@@ -26,16 +27,25 @@ function clearResult() {
 }
 
 function displayResult() {
-  const displayContainer = document.querySelector(".enter-display");
+  const displayContainer = document.querySelector(".result-display");
+  const input = document.querySelector("input");
 
+  const containerItem = document.createElement("section");
+  const operands = document.createElement("div");
   const equalOperator = document.createElement("div");
   const calcResult = document.createElement("div");
 
+  containerItem.style.cssText =
+    "display: flex; justify-content: space-between;";
+
+  operands.textContent = input.value;
   equalOperator.textContent = "=";
   calcResult.textContent = calcValues.result;
 
-  displayContainer.appendChild(equalOperator);
-  displayContainer.appendChild(calcResult);
+  containerItem.appendChild(operands);
+  containerItem.appendChild(equalOperator);
+  containerItem.appendChild(calcResult);
+  displayContainer.appendChild(containerItem);
 }
 
 function clearCalc() {
@@ -47,7 +57,7 @@ function clearCalc() {
 
   clearButton.addEventListener("click", () => {
     for (let key in calcValues) {
-      calcValues[key] = '';
+      calcValues[key] = "";
     }
 
     input.value = "";
@@ -80,9 +90,9 @@ function displayValues() {
 
   buttons.forEach((btn) =>
     btn.addEventListener("click", () => {
-      input.value = Object.values(calcValues)
-        .filter((val) => val !== '')
-        .join(" ");
+      input.value = Object.keys(calcValues)
+        .filter((val) => val !== "result")
+        .reduce((calc, key) => calc + calcValues[key], "");
     })
   );
 }
